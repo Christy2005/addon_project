c3107f66fb587de5df374c13aeb5e8b2029fb6fe
Christy2005 <christydenees123@gmail.com>
2026-06-30 12:45:19 +0530

    multipart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [sources, setSources] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const BACKEND = "http://34.70.21.37:8000";
+  const BACKEND = process.env.REACT_APP_BACKEND;
 
   async function uploadPDF() {
     if (!file) {
